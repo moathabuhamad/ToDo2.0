8213f02ce697f6faa0dbba18ea9ef14e37e5a3ee
moathabuhamad <moathabuhamad@outlook.com>
2022-01-24 20:07:45 +0200

    new user interaction

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,79 @@
 "use strict";
 
-let Name;
-let Gender;
-let Age;
+let inputName;
+let inputGender = "";
+let inputAge;
 let skip;
-let gendercorrect;
+let genderValid;
+let answers = ["", "", ""];
+let questions = [
+  "Do you like Sports\nYes/No?",
+  "Do you like Reading Books\nYes/No?",
+  "Do you like your TA at ASAC\nYes/Yes?",
+];
 
-Name = prompt("Please enter your name");
-Gender = prompt("Please enter you gender\nMale/Female");
-gendercorrect =
-  Gender.toLowerCase() === "male" || Gender.toLowerCase() === "female"
-    ? true
-    : false;
-Age = prompt("How old are you?");
-Age <= 0 ? alert("Age isn't correct") : (Age = Age);
-skip = confirm("skip welcoming message?");
-skip === false
-  ? gendercorrect === true
-    ? Gender.toLowerCase() === "male"
-      ? alert("Welcome Mr. " + Name)
-      : alert("Welcome Ms. " + Name)
-    : alert("Welcome " + Name)
-  : (skip = skip);
+function promptName() {
+  inputName = prompt("Please enter your name");
+}
+function promptGender() {
+  while (
+    inputGender.toLowerCase() != "male" &&
+    inputGender.toLowerCase() != "female"
+  ) {
+    inputGender = prompt("Please enter you gender\nMale/Female");
+    if (inputGender == null) {
+      inputGender = "";
+      break;
+    }
+  }
+  genderValid =
+    inputGender.toLowerCase() === "male" || inputGender.toLowerCase() === "female"
+      ? true
+      : false;
+}
+
+function promptQuestions() {
+  for (let i = 0; i < 3; i++) {
+    while (
+      answers[i].toLowerCase() != "yes" &&
+      answers[i].toLowerCase() != "no" &&
+      answers[i] != "Invalid"
+    ) {
+      answers[i] = prompt(questions[i]);
+      checkAnswers(i);
+    }
+  }
+}
+function checkAnswers(x) {
+  if (answers[x] == null) {
+    answers[x] = "Invalid";
+  }
+}
+function printAnswers() {
+  for (var i = 0; i < 3; i++) {
+    console.log(questions[i] + " " + answers[i]);
+  }
+}
+
+function promptAge() {
+  inputAge = prompt("How old are you?");
+  inputAge <= 0 ? alert("Age isn't valid") : (inputAge = inputAge);
+}
+function greetingMessage() {
+  skip = confirm("skip welcoming message?");
+  skip === false
+    ? genderValid === true
+      ? inputGender.toLowerCase() === "male"
+        ? alert("Welcome Mr. " + inputName)
+        : alert("Welcome Ms. " + inputName)
+      : alert("Welcome " + inputName)
+    : (skip = skip);
+}
+
+promptName();
+promptGender();
+promptAge();
+greetingMessage();
+promptQuestions();
+checkAnswers();
+printAnswers();
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ToDo2.0
+
+<H3>Task5<H3>
 improved version of ToDo
 ***************************************************************************************
 What observations or questions do you have about what you’ve learned so far?
@@ -7,6 +9,18 @@ nothing.
 How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
 this assignment took me 2 hours because i had to study the material as well
 ***************************************************************************************
+
+<H3>Task6<H3>
+added user interactions
+***************************************************************************************
+What observations or questions do you have about what you’ve learned so far?
+everything is going great.
+***************************************************************************************
+How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
+this assignment took me 1 hour.
+
+
+
 ![Homepage](assets/Index.png)
 
 ![About Page](assets/About.png)
